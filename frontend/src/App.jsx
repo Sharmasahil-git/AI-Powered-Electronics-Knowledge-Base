ce2b383f4756e4f49daa55301c6854e327e44d91
akansha02-byte <akanshabhagat428@gmail.com>
2026-06-29 14:02:34 +0530

    Setup React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer/>
     </>
   );
 }
